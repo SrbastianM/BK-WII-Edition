3735287fa9e4b2642332b1d6c72f4cb3e0cf8025
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-03-08 14:58:39 -0500

    cr/home: I finish the quiz desing and start the result desing for a result page

--- app/src/main/java/com/srbastian/bk_wiiedition/view/FragmentQuiz.kt
+++ app/src/main/java/com/srbastian/bk_wiiedition/view/FragmentQuiz.kt
@@ -6,13 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.srbastian.bk_wiiedition.R
+import com.srbastian.bk_wiiedition.databinding.FragmentQuizBinding
 
 class FragmentQuiz : Fragment() {
+    lateinit var fragmentQuizBinding : FragmentQuizBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        fragmentQuizBinding = FragmentQuizBinding.inflate(inflater, container, false)
+        fragmentQuizBinding.btnA.setOnClickListener {  }
+        fragmentQuizBinding.btnB.setOnClickListener {  }
+        fragmentQuizBinding.btnC.setOnClickListener {  }
+        fragmentQuizBinding.btnD.setOnClickListener {  }
+        fragmentQuizBinding.btnNext.setOnClickListener {  }
+
+        return fragmentQuizBinding.root
     }
 }
